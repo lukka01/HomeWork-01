fdbf5c9fc6048381a6c48bc516f0ac159fc2ad4c
luka <khurtsidzeluka0@gmail.com>
2022-12-19 00:42:20 +0400

    HomeWork 1

---
+++ homeW.js
@@ -0,0 +1,19 @@
+//problem 1
+let name = 'luka';
+let surName = 'khurtsidze';
+let age = '17';
+let sex = 'Male';
+let birthDate = '30.06.2005';
+
+console.log("I am " + name + " "  + surName + " and i'm " + age + " years old");
+
+
+//problem2
+
+(100 - (10 +15)/5)*3;
+//there are 9 expressions
+
+//problem3
+
+let mod = 101 % 2;
+console.log(mod);
